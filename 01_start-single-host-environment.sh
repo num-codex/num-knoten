57a41d57131cb4086c2c67f97cd353100d493b77
Marvin Kampf <marvin.kampf@fau.de>
2021-02-01 16:26:34 +0100

    feat: add fdw for fhirgw<>i2b2

--- 01_start-single-host-environment.sh
+++ 01_start-single-host-environment.sh
@@ -9,3 +9,6 @@ docker wait ${COMPOSE_PROJECT}_gpasinit-patient_1 && docker wait ${COMPOSE_PROJE
 docker-compose -p $COMPOSE_PROJECT -f monitoring/docker-compose.yml up -d
 
 docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml up -d
+
+sleep 20
+docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=demouser i2b2pg psql -U i2b2 -d i2b2 -a -f data/create-fdw.sql
