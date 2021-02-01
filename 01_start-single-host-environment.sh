8402e843d41ab7effeae90e5343f207d918ae553
Marvin Kampf <marvin.kampf@fau.de>
2021-02-01 18:22:42 +0100

    feat: adds working pipeline from odm2fhir to fwd

--- 01_start-single-host-environment.sh
+++ 01_start-single-host-environment.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+export COMPOSE_IGNORE_ORPHANS=True
 export COMPOSE_PROJECT=num-knoten
+
+docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml up -d
 
 docker-compose -p $COMPOSE_PROJECT -f fhir-gw/docker-compose.yml up -d
 
 docker wait ${COMPOSE_PROJECT}_gpasinit-patient_1 && docker wait ${COMPOSE_PROJECT}_gpasinit-fall_1
-
 docker-compose -p $COMPOSE_PROJECT -f monitoring/docker-compose.yml up -d
 
-docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml up -d
-
-sleep 20
-docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=demouser i2b2pg psql -U i2b2 -d i2b2 -a -f data/create-fdw.sql
+docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg psql -U postgres -d i2b2 -a -f /create-fdw.sql
