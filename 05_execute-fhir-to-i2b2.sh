c2052c8f3ff7a458dc2cda63a1eef1ba4be26a83
Sebastian Mate <sebastian.mate@uk-erlangen.de>
2021-02-08 16:37:10 +0100

    Fix execution of i2b2 ETL

--- 05_execute-fhir-to-i2b2.sh
+++ 05_execute-fhir-to-i2b2.sh
@@ -2,12 +2,14 @@
 
 export COMPOSE_PROJECT=num-knoten
 
-docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Create Foreign Data Wrapper to FHIR-DB...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/create-fdw.sql"
-docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Creating i2b2 FHIR Trigger...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/i2b2-FHIR-Trigger.sql"
-docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Loading i2b2 Ontology...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/i2b2Ontology.sql"
-docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=demouser i2b2-pg sh -c "echo '### Running Postprocessing.sql...'; psql -U postgres -d i2b2 -a -c 'insert into i2b2miracum.fhir(fhir) select data from i2b2miracum.resources'"
+#docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Creating Foreign Data Wrapper to FHIR-DB ...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/create-fdw.sql > /dev/null"
+#docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Creating i2b2 FHIR Trigger ...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/i2b2-FHIR-Trigger.sql > /dev/null"
+#docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Loading i2b2 Ontology ...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/i2b2Ontology.sql > /dev/null"
+#docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=demouser i2b2-pg sh -c "echo '### Running Postprocessing.sql ...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/Postprocessing.sql > /dev/null"
+#docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=demouser i2b2-pg sh -c "echo '### Executing actual ETL ...'; psql -U postgres -d i2b2 -a -c 'insert into i2b2miracum.fhir(fhir) select data from i2b2miracum.resources' > /dev/null"
 
-# winpty docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Create Foreign Data Wrapper to FHIR-DB...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/create-fdw.sql"
-# winpty docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Creating i2b2 FHIR Trigger...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/i2b2-FHIR-Trigger.sql"
-# winpty docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Loading i2b2 Ontology...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/i2b2Ontology.sql"
-# winpty docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=demouser i2b2-pg sh -c "echo '### Running Postprocessing.sql...'; psql -U postgres -d i2b2 -a -c 'insert into i2b2miracum.fhir(fhir) select data from i2b2miracum.resources'"
+ winpty docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Creating Foreign Data Wrapper to FHIR-DB ...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/create-fdw.sql > /dev/null"
+ winpty docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Creating i2b2 FHIR Trigger ...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/i2b2-FHIR-Trigger.sql > /dev/null"
+ winpty docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=postgres i2b2-pg sh -c "echo '### Loading i2b2 Ontology ...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/i2b2Ontology.sql > /dev/null"
+ winpty docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=demouser i2b2-pg sh -c "echo '### Running Postprocessing.sql ...'; psql -U postgres -d i2b2 -a -f /fhir-to-i2b2/Postprocessing.sql > /dev/null"
+ winpty docker-compose -p $COMPOSE_PROJECT -f i2b2/docker-compose.yml exec -e PGPASSWORD=demouser i2b2-pg sh -c "echo '### Executing actual ETL ...'; psql -U postgres -d i2b2 -a -c 'insert into i2b2miracum.fhir(fhir) select data from i2b2miracum.resources' > /dev/null"
